893bd412e6bca4fdb0771b6b5e31b905dabc3cb1
Jon Christensen <jon@jychri.com>
2019-02-26 14:13:25 -0600

    repos_test is again testing (!), now in 1/4 time

--- pkg/repos/repos_test.go
+++ pkg/repos/repos_test.go
@@ -1,9 +1,8 @@
 package repos
 
 import (
-	// "log"
 	"os"
-	// "strings"
+	"strings"
 	"testing"
 
 	"github.com/jychri/git-in-sync/pkg/atp"
@@ -80,37 +79,36 @@ func TestVerifyChanges(t *testing.T) {
 		{"repos-changes", "tmpgis"},
 	} {
 		// p, cleanup := atp.Hub(tr.pkg, tr.k)
-		// p, _ := atp.Hub(tr.scope, tr.k)
-		atp.Hub(tr.scope, tr.k)
-		// ti := timer.Init()
-		// f := flags.Testing(p)
-		// c := conf.Init(f)
-		// st := stat.Init()
-		// rs := Init(c, f, st, ti)
+		p, _ := atp.Hub(tr.scope, tr.k)
+		ti := timer.Init()
+		f := flags.Testing(p)
+		c := conf.Init(f)
+		st := stat.Init()
+		rs := Init(c, f, st, ti)
 
 		// defer cleanup()
 
-		// rs.VerifyWorkspaces(f, st, ti)
-		// rs.VerifyRepos(f, st, ti)
+		rs.VerifyWorkspaces(f, st, ti)
+		rs.VerifyRepos(f, st, ti)
 
 		// setup
-		// for _, r := range rs {
+		for _, r := range rs {
 
-		// 	if trim := strings.TrimPrefix(r.Name, "gis-"); trim != r.Status {
-		// 		t.Errorf("VerifyChanges: %v mismatch: %v != %v", r.Name, trim, r.Status)
-		// 	}
+			if trim := strings.TrimPrefix(r.Name, "gis-"); trim != r.Status {
+				t.Errorf("VerifyChanges: %v mismatch: %v != %v", r.Name, trim, r.Status)
+			}
 
-		// 	r.Category = "Scheduled"
-		// 	r.Message = "Test commit"
-		// }
+			r.Category = "Scheduled"
+			r.Message = "'TESTVERIFY' commit"
+		}
 
-		// rs.changesAsync(f, st, ti)
-		// rs.infoAsync(f, ti)
+		rs.changesAsync(f, st, ti)
+		rs.infoAsync(f, ti)
 
-		// for _, r := range rs {
-		// 	if r.Status != "Complete" {
-		// 		t.Errorf("VerifyChanges: %v %v != Complete", r.Name, r.Status)
-		// 	}
-		// }
+		for _, r := range rs {
+			if r.Status != "Complete" {
+				t.Errorf("VerifyChanges: %v %v != Complete", r.Name, r.Status)
+			}
+		}
 	}
 }
